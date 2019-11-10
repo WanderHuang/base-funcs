2715eecfadb55cd6a0e80145213037813da9ec25
wander <junjiehuang@yeah.net>
2019-11-10 11:22:35 +0800

    feat(index): 增加hasProperty & reduceProps

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import isType from './fp/isType';
 import pipe from './fp/pipe';
 import isEqual from './fp/isEqual';
 import partial from './fp/partial';
+import hasProperty from './fp/hasProperty';
+import reduceProps from './fp/reduceProps';
 
-export { isType, pipe, isEqual, partial };
+export { isType, pipe, isEqual, partial, hasProperty, reduceProps };
